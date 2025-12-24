f619f7620d189b0b8f0fb4445bb76d58fc98fbbb
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-25 00:38:07 +0800

    Add start handler to fetch data from REPLIT_URL

---
+++ api/start.js
@@ -0,0 +1,7 @@
+import { REPLIT_URL } from "./_config.js"
+
+export default async function handler(req, res) {
+  const r = await fetch(`${REPLIT_URL}/start`)
+  const j = await r.json()
+  res.json(j)
+}
