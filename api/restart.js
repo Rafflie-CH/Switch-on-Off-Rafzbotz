8ecd33b099b32b39ea7e51f9ac2114b25be7eba4
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-25 00:38:42 +0800

    Add API endpoint for restarting service

---
+++ api/restart.js
@@ -0,0 +1,7 @@
+import { REPLIT_URL } from "./_config.js"
+
+export default async function handler(req, res) {
+  const r = await fetch(`${REPLIT_URL}/restart`)
+  const j = await r.json()
+  res.json(j)
+}
